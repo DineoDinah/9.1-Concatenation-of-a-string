ad21f7e3b1879cb1585c858f1e84127cea4353f6
Lenovo-User <Lenovo-User@DESKTOP-RNPFAS5>
2023-10-04 17:39:15 +0200

    Add project files.

---
+++ 9.1 Concatenation of a string/Form1.Designer.vb
@@ -0,0 +1,67 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+	Inherits System.Windows.Forms.Form
+
+	'Form overrides dispose to clean up the component list.
+	<System.Diagnostics.DebuggerNonUserCode()>
+	Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+		Try
+			If disposing AndAlso components IsNot Nothing Then
+				components.Dispose()
+			End If
+		Finally
+			MyBase.Dispose(disposing)
+		End Try
+	End Sub
+
+	'Required by the Windows Form Designer
+	Private components As System.ComponentModel.IContainer
+
+	'NOTE: The following procedure is required by the Windows Form Designer
+	'It can be modified using the Windows Form Designer.  
+	'Do not modify it using the code editor.
+	<System.Diagnostics.DebuggerStepThrough()>
+	Private Sub InitializeComponent()
+		BtnShow = New Button()
+		Label1 = New Label()
+		SuspendLayout()
+		' 
+		' BtnShow
+		' 
+		BtnShow.Location = New Point(252, 244)
+		BtnShow.Margin = New Padding(5, 4, 5, 4)
+		BtnShow.Name = "BtnShow"
+		BtnShow.Size = New Size(179, 41)
+		BtnShow.TabIndex = 0
+		BtnShow.Text = "Button1"
+		BtnShow.UseVisualStyleBackColor = True
+		' 
+		' Label1
+		' 
+		Label1.AutoSize = True
+		Label1.Location = New Point(190, 55)
+		Label1.Margin = New Padding(5, 0, 5, 0)
+		Label1.Name = "Label1"
+		Label1.Size = New Size(290, 30)
+		Label1.TabIndex = 1
+		Label1.Text = "Concatenating String"
+		' 
+		' Form1
+		' 
+		AutoScaleDimensions = New SizeF(16F, 30F)
+		AutoScaleMode = AutoScaleMode.Font
+		BackColor = SystemColors.ControlDark
+		ClientSize = New Size(1280, 540)
+		Controls.Add(Label1)
+		Controls.Add(BtnShow)
+		Font = New Font("Showcard Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point)
+		Margin = New Padding(5, 4, 5, 4)
+		Name = "Form1"
+		Text = "Concatenation string"
+		ResumeLayout(False)
+		PerformLayout()
+	End Sub
+
+	Friend WithEvents BtnShow As Button
+	Friend WithEvents Label1 As Label
+End Class
---
+++ 9.1 Concatenation of a string/Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+	Private Sub BtnShow_Click(sender As Object, e As EventArgs) Handles BtnShow.Click
+		Dim text1 As String
+		Dim text2 As String
+		Dim text3 As String
+		Dim text4 As String
+		text1 = "Visual"
+		text2 = "Basic"
+		text3 = "2022"
+		text4 = text1 + " " + text2 + " " + text3
+		MsgBox(text4)
+	End Sub
+
+
+End Class
